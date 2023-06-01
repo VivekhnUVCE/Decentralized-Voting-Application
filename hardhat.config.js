48630ab8ab2094145b2b368a8f317bca8a110527
Your Name <you@example.com>
2023-06-02 00:19:29 +0530

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,23 @@
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
+
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+
+const { API_URL, PRIVATE_KEY } = process.env;
+
+module.exports = {
+   solidity: "0.8.11",
+   defaultNetwork: "goerli",
+   networks: {
+      hardhat: {},
+      goerli: {
+         url: API_URL,
+         accounts: [`0x${PRIVATE_KEY}`],
+         gas: 2100000,
+         gasPrice: 8000000000,
+         chainId: 5
+      }
+   },
+}
